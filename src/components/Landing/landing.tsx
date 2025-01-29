a550e8c780da5abcd6c7cb4b238424e8825fbe47
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2025-01-28 22:27:51 -0500

    updated setup

--- src/components/Landing/landing.tsx
+++ src/components/Landing/landing.tsx
@@ -1,6 +1,6 @@
 const Landing = () => {
     return(
-        <div className="h-screen w-screen">
+        <div className="h-screen w-screen flex items-center justify-center">
             this is the landing
         </div>
     );
--- src/components/NavBar/navbar.tsx
+++ src/components/NavBar/navbar.tsx
@@ -1,11 +1,18 @@
 const Navbar = () => {
     return(
-        <div className="flex border-y justify-between w-full p-4">
-            <p>About Us</p>
-            <p>What We Do</p>
-            <p>Team</p>
-            <p>Values</p>
-            <p>Contact Us</p>
+        <div className="fixed top-0 border-b w-full p-4 bg-[#203444] text-white">
+            <div className="w-full max-w-[80%] flex justify-between m-auto">
+                <div>
+                    <p className="">LOGO</p>
+                </div>
+                <div className="flex gap-4">
+                    <p className="">About Us</p>
+                    <p className="">What We Do</p>
+                    <p className="">Team</p>
+                    <p className="">Values</p>
+                    <p className="">Contact Us</p>
+                </div>
+            </div>
         </div>
     )
 }
